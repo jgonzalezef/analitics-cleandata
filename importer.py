b8b78e7147e7ce92f6547f0d966d83d7ed85b116
jgonzalezef <jesus.gonzalez@enviaflores.com>
2024-04-17 10:25:00 -0600

    importador

---
+++ importer.py
@@ -0,0 +1,13 @@
+# Importa los scripts
+import subprocess
+
+# Ejecuta los archivos en orden
+subprocess.run(["python3", "1_planes.py"])
+subprocess.run(["python3", "2_asignaturas.py"])
+subprocess.run(["python3", "3_calificaciones.py"])
+subprocess.run(["python3", "5_docentes.py"])
+subprocess.run(["python3", "6_grupos.py"])
+subprocess.run(["python3", "7_calificaciones_grupos.py"])
+subprocess.run(["python3", "8_tutores.py"])
+subprocess.run(["python3", "9_estudiante_persona.py"])
+subprocess.run(["python3", "10_estudiantes_estatus.py"])
